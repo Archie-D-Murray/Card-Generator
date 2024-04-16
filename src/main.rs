7b50b9908da50d129a407d46c103ee8bfccc62db
Archie-Murray <76486169+Archie-Murray@users.noreply.github.com>
2024-04-16 17:54:44 +0100

    feat: Added debug to power budget

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use rand::Rng;
 
 const DEFAULT_PRIORITY: u32 = 11;
 
-fn apply_multiplier(value: u32, multiplier: f32) -> u32 {
-    (value as f32 * multiplier).floor() as u32
+fn apply_multiplier(value: i32, multiplier: f32) -> i32 {
+    (value as f32 * multiplier).floor() as i32
 }
 
 enum Rarity {
@@ -16,7 +16,7 @@ enum Rarity {
     Great,
 }
 
-fn cost_from_rarity(rarity: &Rarity) -> Vec<u32> {
+fn cost_from_rarity(rarity: &Rarity) -> Vec<i32> {
     match rarity {
         Rarity::Bad => vec![1, 2],
         Rarity::NotGreat => vec![3, 4],
@@ -42,23 +42,23 @@ fn multiplier_from_efficiency(efficiency: &Efficiency) -> f32 {
 
 #[derive(Clone, Debug)]
 enum Effect {
-    Heal(u32),
-    AcidHeal(u32),
-    Damage(u32),
-    DoT(u32, u32),
+    Heal(i32),
+    AcidHeal(i32),
+    Damage(i32),
+    DoT(i32, i32),
 }
 
-fn cost_from_effect(effect: Effect, budget: u32) -> (Effect, u32) {
+fn cost_from_effect(effect: Effect, budget: i32) -> (Effect, i32) {
     match effect {
         Effect::Heal(_) => {
-            let amount = (budget as f32 / 1.5).floor() as u32;
-            (Effect::Heal(amount), (amount as f32 * 1.5).floor() as u32)
+            let amount = (budget as f32 / 1.5).floor() as i32;
+            (Effect::Heal(amount), (amount as f32 * 1.5).floor() as i32)
         }
         Effect::AcidHeal(_) => {
-            let amount = (budget as f32 / 1.75).floor() as u32;
+            let amount = (budget as f32 / 1.75).floor() as i32;
             (
                 Effect::AcidHeal(amount),
-                (amount as f32 * 1.75).floor() as u32,
+                (amount as f32 * 1.75).floor() as i32,
             )
         }
         Effect::Damage(_) => (Effect::Damage(budget), budget),
@@ -77,7 +77,7 @@ enum Range {
     ExtendedAoE,
 }
 
-fn cost_from_range(range: Range) -> u32 {
+fn cost_from_range(range: Range) -> i32 {
     match range {
         Range::Single => 0,
         Range::Multiple => 1,
@@ -88,9 +88,9 @@ fn cost_from_range(range: Range) -> u32 {
 
 #[derive(Clone, Debug)]
 struct Card {
-    budget: u32,
+    budget: i32,
     priority: u32,
-    barnacles: u32,
+    barnacles: i32,
     budget_share: (f32, f32),
     range: Option<Range>,
     effect: Option<Effect>,
@@ -103,11 +103,11 @@ where
     value >= min && value <= max
 }
 
-fn priority_from_budget(budget: u32) -> u32 {
-    if budget == 0 {
-        11
+fn priority_from_budget(budget: i32) -> i32 {
+    if budget <= 0 {
+        0
     } else {
-        apply_multiplier(10, (budget as f32 / 10.0).min(1.0))
+        apply_multiplier(10, (1.0 - budget as f32 / 10.0).max(1.0))
     }
 }
 
@@ -136,14 +136,14 @@ impl Card {
 
     pub fn with_effect(&mut self, effect: Effect) -> &mut Card {
         let (created_effect, used) =
-            cost_from_effect(effect, apply_multiplier(self.budget, self.budget_share.0));
+            cost_from_effect(effect, apply_multiplier(self.budget.max(0), self.budget_share.0));
         self.budget -= used;
         self.effect = Some(created_effect);
         self
     }
 
     pub fn build(&mut self) -> Result<Card, String> {
-        self.priority = priority_from_budget(self.budget);
+        self.priority -= priority_from_budget(self.budget) as u32;
         if self.priority == DEFAULT_PRIORITY || self.barnacles == 0 {
             Err(String::from(format!(
                 "Card prio {} due to budget: {}",
@@ -159,15 +159,16 @@ fn main() {
     let rarity = get_rarity();
     let efficiency = get_efficiency();
     let effect_share = get_effect_share();
-    let range = get_range();
-    let effect = get_effect();
-    let card_result = Card::new(rarity, efficiency, effect_share)
-        .with_range(range)
-        .with_effect(effect)
-        .build();
+    let mut card = Card::new(rarity, efficiency, effect_share);
+    println!("Created card with power budget: {} and split {}/{}", card.budget, card.budget_share.0, card.budget_share.1);
+    card.with_range(get_range());
+    println!("New budget: {}", card.budget);
+    card.with_effect(get_effect());
+    println!("New budget: {}", card.budget);
+    let card_result = card.build();
     match card_result {
         Ok(card) => println!("{:?}", card),
-        Err(msg) => println!("{}", msg),
+        Err(err) => eprintln!("ERROR: {}", err),
     }
 }
 
@@ -196,22 +197,22 @@ where
 }
 
 fn get_effect_share() -> f32 {
-    get_num(0.0, 1.0, String::from("Enter effect share: (0.0..1.0)\t"))
+    get_num(0.0, 1.0, String::from("Enter effect share: (0.0..1.0).. "))
 }
 
 fn get_effect() -> Effect {
-    let effect_type: u32 = get_num(
+    let effect_type: i32 = get_num(
         1,
         4,
         String::from(
-            "Enter effect type: \n1: Damage\n2: Heal\n3: DoT\n4: Acid Healing\n(1..4)..\t",
+            "1: Damage\n2: Heal\n3: DoT\n4: Acid Healing\nEnter effect type: (1..4).. ",
         ),
     ) - 1;
     match effect_type {
         0 => Effect::Damage(0),
         1 => Effect::Heal(0),
         2 => {
-            let turns: u32 = get_num(2, 4, String::from("Enter turn duration (2..4):\t"));
+            let turns: i32 = get_num(2, 4, String::from("Enter turn duration (2..4):\t"));
             Effect::DoT(0, turns)
         }
         3 => Effect::AcidHeal(0),
@@ -220,7 +221,7 @@ fn get_effect() -> Effect {
 }
 
 fn get_range() -> Range {
-    match get_num(1, 4, String::from("Enter range type: \n1: Single\n2: Multiple (2)\n3: AoE (room)\n4: AoE (Extended)\n(1..4)..\t")) - 1u32 {
+    match get_num(1, 4, String::from("1: Single\n2: Multiple (2)\n3: AoE (room)\n4: AoE (Extended)\nEnter range type: (1..4).. ")) - 1i32 {
         0 => Range::Single,
         1 => Range::Multiple,
         2 => Range::AoE,
@@ -233,8 +234,8 @@ fn get_efficiency() -> Efficiency {
     match get_num(
         1,
         3,
-        String::from("Enter efficiency: \n1: Bad\n2: Normal\n3: Good\n(1..3)..\t"),
-    ) - 1u32
+        String::from("1 Bad\n2: Normal\n3: Good\nEnter efficiency: (1..3).. "),
+    ) - 1i32
     {
         0 => Efficiency::Bad,
         1 => Efficiency::Normal,
@@ -248,9 +249,9 @@ fn get_rarity() -> Rarity {
         1,
         5,
         String::from(
-            "Enter rarity type: \n1: Bad\n2: Not Great\n3: Normal\n4: Good\n5: Great\n(1..5)..\t",
+            "1: Bad\n2: Not Great\n3: Normal\n4: Good\n5: Great\nEnter rarity type: (1..5).. ",
         ),
-    ) - 1u32
+    ) - 1i32
     {
         0 => Rarity::Bad,
         1 => Rarity::NotGreat,
